a0e5dd725bd788efa056d818e8e8610765eb05f4
Даниил Кенсовский <kesowski@MacBook-Air-Daniil.local>
2022-06-01 13:40:21 +0300

    delete commit

--- src/client/assets/javascripts/app/App.js
+++ src/client/assets/javascripts/app/App.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 
 const App = (props) => (
   <div className="page-container">
-    {/* {React.cloneElement({...props}.children, {...props})} */}
     {{...props}.children}
   </div>
 );
